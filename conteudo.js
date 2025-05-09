8f928a24c2c12c1eef4c3e9fc7fd532fea13478e
EricaMkleinkauf <162368774+EricaMkleinkauf@users.noreply.github.com>
2025-05-09 16:45:07 -0300

    Add files via upload

---
+++ conteudo.js
@@ -0,0 +1,9 @@
+/*function validarBusca(){
+    if(document.querySelector('#inputlupa').value=="");{
+        alert("Nao podia ter dexando em branco a busca!!")
+        return false;
+    }
+}
+
+document.querySelector("#formbusca").onsubmit = validarBusca;
+*/
